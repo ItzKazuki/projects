39e3c85c343555a72388e97306fffce3da018276
ItzKazuki <ibnu235729@gmail.com>
2024-04-19 19:29:23 +0700

    add MyMovies at /projects/my-movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
 import Bullying from "./pages/Bullying";
 import Layout from "./components/Layout";
 import CheckDevice from "./middleware/CheckDevice";
 import ErrorPage from "./pages/ErrorPage";
+import MyMovies, { MovieSearch } from "./pages/MyMovies";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHomeAlt,
+  faInfoCircle,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function App() {
   return (
@@ -18,8 +25,57 @@ export default function App() {
               </Layout>
             }
           />
+          <Route
+            path="my-movie"
+            element={
+              <MyMoviesLayout>
+                <MyMovies />
+              </MyMoviesLayout>
+            }
+          />
+          <Route
+            path="my-movie/search"
+            element={
+              <MyMoviesLayout>
+                <MovieSearch />
+              </MyMoviesLayout>
+            }
+          />
         </Route>
       </Route>
     </Routes>
+  );
+}
+
+function MyMoviesLayout({ children }) {
+  // check navbar page
+  //get pathname
+  const { pathname } = useLocation();
+  let currentPath = pathname.split("/")[3];
+  return (
+    <>
+      <div className="navbar border-b top-0 sticky z-10 bg-base-100">
+        <div className="navbar-start">
+          {currentPath == "search" ? (
+            <Link to={"/projects/my-movie"} className="btn btn-ghost btn-circle">
+              <FontAwesomeIcon className="h-5 w-5" icon={faHomeAlt} />
+            </Link>
+          ) : (
+            <Link to={"search"} className="btn btn-ghost btn-circle">
+              <FontAwesomeIcon className="h-5 w-5" icon={faSearch} />
+            </Link>
+          )}
+        </div>
+        <div className="navbar-center">
+          <a className="btn btn-ghost text-lg">MyMovies</a>
+        </div>
+        <div className="navbar-end">
+          <Link className="btn btn-ghost btn-circle">
+            <FontAwesomeIcon icon={faInfoCircle} className="h-5 w-5" />
+          </Link>
+        </div>
+      </div>
+      <div className="my-4 mx-6 min-h-screen">{children}</div>
+    </>
   );
 }
---
+++ src/pages/MyMovies.jsx
@@ -0,0 +1,164 @@
+import { useEffect, useState } from "react";
+import { Card, Each } from "../components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
+const url = "https://api.themoviedb.org/3";
+const apiKey = import.meta.env.VITE_API_MOVIEDB;
+const apiKeyBearer = import.meta.env.VITE_API_MOVIEDB_BEARER;
+
+let headers = {
+  accept: "application/json",
+  Authorization: `Bearer ${apiKeyBearer}`,
+};
+
+const getData = async (path) => {
+  const resServ = await fetch(`${url}${path}`, {
+    method: "GET",
+    headers,
+  });
+
+  return resServ.json();
+};
+
+export default function MyMovies() {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    // fetch api here
+    getData("/movie/popular?language=en-EN&page=1").then((res) =>
+      setPopularMovies(res.results)
+    );
+  }, []);
+
+  return (
+    <div>
+      <div id="popular" className="mb-3">
+        <h1 className="text-2xl font-bold mb-4">Popular</h1>
+        <div className="flex flex-nowrap overflow-auto">
+          <Each of={popularMovies} render={(item) => <Movie detail={item} />} />
+        </div>
+      </div>
+      <div id="now-playing">
+        <h1 className="text-2xl font-bold mb-4">Now Playing</h1>
+      </div>
+    </div>
+  );
+}
+
+function Movie({ detail, className }) {
+  const [resMovie, setResMovie] = useState();
+
+  const handleCloseModal = () => {
+    document.getElementById(`detail_movie_${detail.id}`).classList.remove('modal-open');
+  }
+
+  const handleClickModal = async () => {
+    //harus fetch di sini, trs masukin ke resMovie, trs open modal
+    await getData(`/movie/${detail.id}`).then(res => setResMovie(res))
+    // open modal here
+    document.getElementById(`detail_movie_${detail.id}`).classList.add('modal-open');
+  }
+
+
+  return (
+    <>
+      {detail.poster_path ? (
+        <img
+        onClick={handleClickModal}
+          src={`https://image.tmdb.org/t/p/original/${detail.poster_path}`}
+          alt=""
+          className={`rounded w-32 mr-2 flex-auto ${className}`}
+        />
+      ) : (
+        ""
+      )}
+        <dialog id={`detail_movie_${detail.id}`} className="modal modal-middle">
+        <div className="modal-box">
+          <img src={`https://image.tmdb.org/t/p/original/${detail.poster_path}`} width={200} alt="" className="rounded mx-auto" />
+          <h3 className="font-bold text-2xl mt-2">{detail.title}</h3>
+          <p className="py-4">
+            {detail.overview}
+          </p>
+          <div className="modal-action">
+            {resMovie ? (<Link to={resMovie.homepage} className="btn btn-primary">Official Web</Link>): ""}
+            <button className="btn" onClick={handleCloseModal}>Close</button>
+          </div>
+        </div>
+      </dialog>
+    </>
+  );
+}
+
+// gini aja ini page utama, jadi kita bakal sort berdasarkan genre horor etc..
+
+const ListGenere = () => {
+  return (
+    <div id="genere">
+      <h1 className="text-2xl font-bold mb-4">Genere</h1>
+      <div className="grid grid-cols-2 gap-2 pl-3">
+        <Card isCenter={true} className={"bg-red-500"}>
+          <h1 className="card-title">Horror</h1>
+        </Card>
+        <Card isCenter={true} className={"bg-red-500"}>
+          <h1 className="card-title">Fiction</h1>
+        </Card>
+        <Card isCenter={true} className={"bg-red-500"}>
+          <h1 className="card-title">Action</h1>
+        </Card>
+        <Card isCenter={true} className={"bg-red-500"}>
+          <h1 className="card-title">Sci-fi</h1>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export function MovieSearch() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResult, setSearchResult] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // do fetch here!
+    getData(`/search/movie?api_key=${apiKey}&query=${searchQuery}`).then(
+      (res) => setSearchResult(res.results)
+    );
+  };
+
+  const SearchResult = () => {
+    return (
+      <div className="flex flex-wrap">
+        <Each
+          of={searchResult}
+          render={(item) => <Movie detail={item} className={"my-2"} />}
+        />
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <div id="search" className="mb-3">
+        <h1 className="text-2xl font-bold mb-2">Looking for?</h1>
+        <form onSubmit={handleSubmit}>
+          <label
+            htmlFor="movie-search"
+            className="input input-md input-bordered flex items-center gap-2">
+            <FontAwesomeIcon icon={faSearch} />
+            <input
+              type="text"
+              className="grow"
+              placeholder="Dua Hati Biru"
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </label>
+          <button type="submit" className="hidden"></button>
+        </form>
+      </div>
+      {searchResult ? <SearchResult /> : <ListGenere />}
+    </div>
+  );
+}
